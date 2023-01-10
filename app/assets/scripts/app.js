a4c5b8cdfe9a81c5117fc6b26e678aefe7c8fa42
Tomeee <diether.noche@gmail.com>
2023-01-10 22:34:12 +0800

    Add a tooltip script

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -3,3 +3,9 @@ import "../styles/styles.css";
 if (module.hot) {
   module.hot.accept();
 }
+
+//Tooltip script
+const tooltips = document.querySelectorAll(".ttips");
+tooltips.forEach((t) => {
+  new bootstrap.Tooltip(t);
+});
