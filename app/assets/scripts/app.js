57e94b7757eb86f5016ad785e036e05c20363384
Tomeee <diether.noche@gmail.com>
2023-01-11 19:06:03 +0800

    Show modal on submit

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -9,3 +9,11 @@ const tooltips = document.querySelectorAll(".ttips");
 tooltips.forEach((t) => {
   new bootstrap.Tooltip(t);
 });
+
+//Clear on submit
+let btnClear = document.querySelector(".btn--clear");
+let inputs = document.querySelectorAll("input");
+
+btnClear.addEventListener("click", () => {
+  inputs.forEach((input) => (input.value = ""));
+});
